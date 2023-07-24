96bd8e7e87f6e9e2c63a955cff14596fa6b813bc
timoxa0 <tima012007@gmail.com>
2023-07-24 16:30:20 +0500

    Add init logger. Add discord embed author support. Fix skin and cape commands help

--- src/main/java/ru/timoxa0/GABot/commands/HelpCommand.java
+++ src/main/java/ru/timoxa0/GABot/commands/HelpCommand.java
@@ -4,6 +4,7 @@ import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import org.apache.logging.log4j.LogManager;
 import ru.timoxa0.GABot.handlers.ConfigHandler;
 import ru.timoxa0.GABot.handlers.TranslationHandler;
 
@@ -12,33 +13,54 @@ import java.awt.*;
 public class HelpCommand extends SlashCommand {
     private final TranslationHandler trn = TranslationHandler.getTranslationHandler();
     private final ConfigHandler cfg = ConfigHandler.getConfigHandler();
+
     public HelpCommand() {
         this.name = "help";
         this.help = trn.getProperty("commands.help.description");
+        LogManager.getLogger(this.getClass()).info(String.format("Added command: /%s", this.name));
     }
 
     @Override
     protected void execute(SlashCommandEvent event) {
-        MessageEmbed embed = new EmbedBuilder()
+        EmbedBuilder embed = new EmbedBuilder()
                 .setTitle(trn.getProperty("help.title"))
                 .setDescription("help.description")
                 .setColor(Color.decode(cfg.getProperty("ds.bot.embed.color")))
-                .addField("/register",      trn.getProperty("help.commands.register"),  true)
+                .addField("/register", trn.getProperty("help.commands.register"), true)
                 .addBlankField(true)
-                .addField("/password",      trn.getProperty("help.commands.password"),  true)
-                .addField("/skin",          trn.getProperty("help.commands.skin"),      true)
+                .addField("/password", trn.getProperty("help.commands.password"), true)
+                .addField("/skin " + trn.getProperty("commands.skin.upload.name"), trn.getProperty("help.commands.skin.upload"), true)
                 .addBlankField(true)
-                .addField("/skin",          trn.getProperty("help.commands.skin"),      true)
-                .addField("/cape",          trn.getProperty("help.commands.cape"),      true)
+                .addField("/skin " + trn.getProperty("commands.skin.mojang.name"), trn.getProperty("help.commands.skin.mojang"), true)
+                .addField("/cape " + trn.getProperty("commands.cape.upload.name"), trn.getProperty("help.commands.cape.upload"), true)
                 .addBlankField(true)
-                .addField("/cape",          trn.getProperty("help.commands.cape"),      true)
-                .addField("/links",         trn.getProperty("help.commands.links"),     true)
+                .addField("/cape " + trn.getProperty("commands.cape.remove.name"), trn.getProperty("help.commands.cape.remove"), true)
+                .addField("/links", trn.getProperty("help.commands.links"), true)
                 .addBlankField(true)
-                .addField("/help",          trn.getProperty("help.commands.help"),      true)
-                .build();
+                .addField("/help", trn.getProperty("help.commands.help"), true);
+
+        if (!cfg.getProperty("ds.bot.embed.author.name", "").isEmpty()) {
+            if (!cfg.getProperty("ds.bot.embed.author.url", "").isEmpty()) {
+                if (!cfg.getProperty("ds.bot.embed.author.icon", "").isEmpty()) {
+                    embed.setAuthor(
+                            cfg.getProperty("ds.bot.embed.author.name"),
+                            cfg.getProperty("ds.bot.embed.author.url"),
+                            cfg.getProperty("ds.bot.embed.author.icon")
+                    );
+                } else {
+                    embed.setAuthor(
+                            cfg.getProperty("ds.bot.embed.author.name"),
+                            cfg.getProperty("ds.bot.embed.author.url")
+                    );
+                }
+            } else {
+                embed.setAuthor(cfg.getProperty("ds.bot.embed.author.name"));
+            }
+        }
+
         event.reply("")
                 .setEphemeral(true)
-                .setEmbeds(embed)
+                .setEmbeds(embed.build())
                 .queue();
     }
 }
