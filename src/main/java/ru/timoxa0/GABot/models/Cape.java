470ec9a541c229913847d4ffc814e940feebec5d
timoxa0 <tima012007@gmail.com>
2023-07-24 11:46:37 +0500

    Removed redundant canonical constructor

--- src/main/java/ru/timoxa0/GABot/models/Cape.java
+++ src/main/java/ru/timoxa0/GABot/models/Cape.java
@@ -3,9 +3,6 @@ package ru.timoxa0.GABot.models;
 import java.io.InputStream;
 
 public record Cape(InputStream stream) {
-    public Cape(InputStream stream) {
-        this.stream = stream;
-    }
 
     @Override
     public InputStream stream() {
--- src/main/java/ru/timoxa0/GABot/models/Skin.java
+++ src/main/java/ru/timoxa0/GABot/models/Skin.java
@@ -3,10 +3,6 @@ package ru.timoxa0.GABot.models;
 import java.io.InputStream;
 
 public record Skin(InputStream stream, boolean isSlim) {
-    public Skin(InputStream stream, boolean isSlim) {
-        this.isSlim = isSlim;
-        this.stream = stream;
-    }
 
     @Override
     public InputStream stream() {
