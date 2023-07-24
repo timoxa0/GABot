6ef9f5d708bc68cd959697e9854b49e856a8adbe
timoxa0 <tima012007@gmail.com>
2023-07-24 16:32:08 +0500

    Add init logger. Rename command /nick -> /name

--- src/main/java/ru/timoxa0/GABot/commands/NameCommand.java
+++ src/main/java/ru/timoxa0/GABot/commands/NameCommand.java
@@ -4,22 +4,24 @@ import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.apache.logging.log4j.LogManager;
 import ru.timoxa0.GABot.handlers.TranslationHandler;
 import ru.timoxa0.GABot.models.MCUser;
 
 import java.util.Collections;
 import java.util.Objects;
 
-public class NickCommand extends SlashCommand {
+public class NameCommand extends SlashCommand {
     private final TranslationHandler trn = TranslationHandler.getTranslationHandler();
 
-    public NickCommand() {
-        this.name = "nick";
-        this.help = trn.getProperty("commands.nick.description");
+    public NameCommand() {
+        this.name = "name";
+        this.help = trn.getProperty("commands.name.description");
         this.options = Collections.singletonList(new OptionData(
-                OptionType.STRING, trn.getProperty("options.nick"),
-                trn.getProperty("options.nick.description")).setRequired(true)
+                OptionType.STRING, trn.getProperty("options.name"),
+                trn.getProperty("options.name.description")).setRequired(true)
         );
+        LogManager.getLogger(this.getClass()).info(String.format("Added command: /%s", this.name));
     }
 
     @Override
@@ -27,12 +29,12 @@ public class NickCommand extends SlashCommand {
         MCUser user = MCUser.getByID(Objects.requireNonNull(Objects.requireNonNull(event.getMember()).getId()));
         if (user != null) {
             if (user.setName(event.getOptions().get(0).getAsString()).commit()) {
-                event.reply(trn.getProperty("commands.nick.success")).setEphemeral(true).queue();
+                event.reply(trn.getProperty("commands.name.success")).setEphemeral(true).queue();
             } else {
                 event.reply(trn.getProperty("misc.command.error")).setEphemeral(true).queue();
             }
         } else {
-            event.reply(trn.getProperty("commands.nick.failed")).setEphemeral(true).queue();
+            event.reply(trn.getProperty("commands.name.failed")).setEphemeral(true).queue();
         }
     }
 }
