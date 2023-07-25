397375a66a6c0e36a06e0e1fb05aa3c786335345
timoxa0 <tima012007@gmail.com>
2023-07-26 01:03:12 +0500

    Add config creation if correct one not found

--- src/main/java/ru/timoxa0/GABot/Main.java
+++ src/main/java/ru/timoxa0/GABot/Main.java
@@ -15,6 +15,7 @@ import sun.misc.Signal;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Scanner;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
@@ -23,15 +24,67 @@ public class Main {
         ConfigHandler cfg = null;
 
         try {
-            cfg = ConfigHandler.getConfigHandler(Path.of("bot.properties"));
+            if (ConfigHandler.createConfigHandler(Path.of("bot.config").toFile())) {
+                try {
+                    run(ConfigHandler.getConfigHandler());
+                } catch (IllegalArgumentException e) {
+                    logger.fatal("Config error: " + e.getMessage());
+                }
+            } else {
+                logger.info("Config file incorrect or not found. Creating new one");
+                createConfig(ConfigHandler.getConfigHandler());
+                System.exit(0);
+            }
         } catch (IOException e) {
             logger.fatal("Failed to load config file!");
             logger.fatal(e.getMessage());
             System.exit(1);
         }
+    }
 
+    private static void createConfig(ConfigHandler cfg) {
+        String tmp;
+
+        cfg.setProperty("ds.bot.token", input("discord bot token"));
+        cfg.setProperty("ds.bot.guild.id", input("discord guild id"));
+        cfg.setProperty("ds.bot.owner.id", input("bot owner id", "0"));
+        cfg.setProperty("ds.bot.translation", input("bot language (en-us | ru)", "ru"));
+        cfg.setProperty("ds.bot.status", input("bot status (online|idle|dnd|invisible|offline)", "online"));
+        cfg.setProperty("ds.bot.activity.type", input("bot activity type (playing|steaming|watching|listening|competing)", "playing"));
+        cfg.setProperty("ds.bot.activity.text", input("bot activity text"));
+        cfg.setProperty("ds.bot.links.exe", input("link to launcher.exe"));
+        cfg.setProperty("ds.bot.links.jar", input("link to launcher.jar"));
+        cfg.setProperty("ds.bot.embed.color", input("bot links and help embeds color", "0x000000"));
+
+        tmp = input("bot embeds author name", "None");
+        if (!tmp.equals("None")) { cfg.setProperty("ds.bot.embed.author.name", tmp); }
+        tmp = input("bot embeds author url", "None");
+        if (!tmp.equals("None")) { cfg.setProperty("ds.bot.embed.author.url", tmp); }
+        tmp = input("bot embeds author icon url", "None");
+        if (!tmp.equals("None")) { cfg.setProperty("ds.bot.embed.author.icon", tmp); }
+
+        cfg.setProperty("db.connectionURL", String.format("jdbc:mariadb://%s:%s/%s",
+                input("database host"),
+                input("database port", "3306"),
+                input("database name")
+        ));
+        cfg.setProperty("db.table", input("database table"));
+        cfg.setProperty("db.username", input("database username"));
+        cfg.setProperty("db.password", input("database password"));
+        cfg.setProperty("db.columns.id", input("database id column name", "id"));
+        cfg.setProperty("db.columns.uuid", input("database uuid column name", "uuid"));
+        cfg.setProperty("db.columns.username", input("database username column name", "username"));
+        cfg.setProperty("db.columns.password", input("database password column name", "password"));
+
+        cfg.setProperty("tp.server.host", input("texture provider host", "127.0.0.1"));
+        cfg.setProperty("tp.server.port", input("texture provider port", "9275"));
+
+        cfg.flush();
+    }
+
+    private static void run(ConfigHandler cfg) throws IllegalArgumentException {
         try {
-            TranslationHandler.createTranslationHandler(Path.of(cfg.getProperty("ds.bot.translation")));
+            TranslationHandler.createTranslationHandler(cfg.getProperty("ds.bot.translation"));
             logger.info("Loaded bot language: " + TranslationHandler.getTranslationHandler().getProperty("translation.lang"));
         } catch (IOException e) {
             logger.fatal("Failed to load translation file!");
@@ -96,11 +149,33 @@ public class Main {
         server.start();
 
         Signal.handle(new Signal("INT"), signal -> {
-            logger.info("Caught SIGINT -> Performing shutdown");
-            jda.shutdownNow();
-            server.stop();
-            System.exit(0);
-        }
+                    logger.info("Caught SIGINT -> Performing shutdown");
+                    jda.shutdownNow();
+                    server.stop();
+                    System.exit(0);
+                }
         );
+    }
+
+    private static String input(String query) {
+        String response;
+        do {
+            System.out.printf("Enter %s: ", query);
+            Scanner sc = new Scanner(System.in);
+            response = sc.nextLine();
+        } while (response.isEmpty());
+        return response.trim();
+    }
+
+    private static String input(String query, String fallback) {
+        String response;
+        System.out.printf("Enter %s [%s]: ", query, fallback);
+        Scanner sc = new Scanner(System.in);
+        response = sc.nextLine();
+        if (response.isEmpty()) {
+            return fallback;
+        } else {
+            return response.trim();
+        }
     }
 }
--- src/main/java/ru/timoxa0/GABot/handlers/ConfigHandler.java
+++ src/main/java/ru/timoxa0/GABot/handlers/ConfigHandler.java
@@ -1,18 +1,38 @@
 package ru.timoxa0.GABot.handlers;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
+import java.util.Objects;
 import java.util.Properties;
 
 public class ConfigHandler {
+    private static final Logger logger = LogManager.getLogger(ConfigHandler.class);
     private static ConfigHandler configHandler = null;
     private final Properties prop = new Properties();
+    private final File config;
+    private final boolean isNew;
 
-    public static synchronized void createConfigHandler(Path configPath) throws IOException {
-        configHandler = new ConfigHandler(configPath);
+    private ConfigHandler(File config) throws IOException {
+        this.config = config;
+        if (validate()) {
+            this.isNew = false;
+            prop.load(new FileReader(config, StandardCharsets.UTF_8));
+        } else {
+            this.isNew = true;
+            try (InputStream cfgRes = ConfigHandler.class.getResourceAsStream("/assets/sample.config")) {
+                assert cfgRes != null;
+                prop.load(new InputStreamReader(cfgRes, StandardCharsets.UTF_8));
+                flush();
+            }
+        }
+    }
+
+    public static synchronized boolean createConfigHandler(File config) throws IOException {
+        configHandler = new ConfigHandler(config);
+        return !configHandler.isNew;
     }
 
     public static synchronized ConfigHandler getConfigHandler() {
@@ -20,21 +40,46 @@ public class ConfigHandler {
         return configHandler;
     }
 
-    public static synchronized ConfigHandler getConfigHandler(Path configPath) throws IOException {
-        createConfigHandler(configPath);
-        return configHandler;
+    private boolean validate() throws IOException {
+        boolean result = false;
+        if (config.exists()) {
+            Properties val_prop = new Properties();
+            Properties sample_prop = new Properties();
+            val_prop.load(new FileReader(config, StandardCharsets.UTF_8));
+            sample_prop.load(new InputStreamReader(
+                    Objects.requireNonNull(ConfigHandler.class.getResourceAsStream("/assets/sample.config")),
+                    StandardCharsets.UTF_8
+            ));
+            result = val_prop.keySet().equals(sample_prop.keySet());
+        }
+        return result;
     }
 
-    private ConfigHandler(Path configPath) throws IOException {
-        File configFile = new File(configPath.toUri());
-        prop.load(new FileReader(configFile, StandardCharsets.UTF_8));
+    public void flush() {
+        try {
+            prop.store(new FileWriter(config, StandardCharsets.UTF_8), "GABot config file");
+        } catch (IOException e) {
+            logger.error("Failed to update config: " + e.getMessage());
+        }
+    }
+
+    public boolean isNew() {
+        return isNew;
     }
 
     public String getProperty(String property) {
-        return prop.getProperty(property);
+        String r = prop.getProperty(property);
+        if (r == null) {
+            logger.warn("Failed to get property: " + property);
+        }
+        return r;
     }
 
     public String getProperty(String property, String defaultValue) {
         return prop.getProperty(property, defaultValue);
+    }
+
+    public void setProperty(String key, String value) {
+        prop.setProperty(key, value);
     }
 }
